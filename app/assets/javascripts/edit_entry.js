fe864113c48c2f6edb8700d673186900078a0dee
thebluber <thebluber@gmail.com>
2013-03-21 20:06:42 +0100

    add click handler to buttons for editable div

--- app/assets/javascripts/edit_entry.js
+++ app/assets/javascripts/edit_entry.js
@@ -1,5 +1,20 @@
+function register_click_handler(button_id, css_class_name) {
+  $(button_id).on("mousedown", function(){
+    text = "<span class='" + css_class_name + "'>" + window.getSelection().toString() + "</span>";
+    document.execCommand("inserthtml", false, text);
+  })
+}
 $(function() {
   $("#uebersetzung_editable").on("input", function(){
     $("#entry_uebersetzung").attr("value", $("#uebersetzung_editable").html())
   });
+  
+  $("#b2").on("mousedown", function(){
+    text = "<span class='werktitel_original'>" + window.getSelection().toString() + "</span>";
+    document.execCommand("inserthtml", false, text);
+  })
+  register_click_handler("#b1", "fachtermini");
+  register_click_handler("#b2", "werktitel_original");
 });
+
+
