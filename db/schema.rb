26a2d191b5af306d7616896166e462046ae3e2f7
thebluber <thebluber@gmail.com>
2013-05-30 14:07:20 +0200

    add freigeschaltet attribute to entries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130506121843) do
+ActiveRecord::Schema.define(:version => 20130530120233) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(:version => 20130506121843) do
     t.datetime "updated_at",                      :null => false
     t.integer  "user_id"
     t.string   "page_reference"
+    t.boolean  "freigeschaltet"
   end
 
   create_table "users", :force => true do |t|
