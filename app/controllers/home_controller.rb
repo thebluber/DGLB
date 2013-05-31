b72cd05626025b682c1e554c3b23213bf60d1a35
timschierbaum <tim.schierbaum@gmail.com>
2013-05-31 13:45:35 +0000

    Added dictionary layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!
+  def index
+    params[:search] = nil if params[:search] and params[:search].strip == "" 
+    @page = params[:page] || 0
+    @entries = (params[:search] ? Entry.search(params[:search]) : Entry).order("japanische_umschrift").page(@page)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @entries }
+    end
+  end
+
 
 end
