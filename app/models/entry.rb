8db01906dbe6a5a0ad38d59e071c1a51181d242c
thebluber <thebluber@gmail.com>
2013-06-01 17:03:23 +0200

    change order to romaji_order in entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,7 +2,7 @@ class Entry < ActiveRecord::Base
   has_paper_trail
   belongs_to :user
   has_many :comments
-  attr_accessible :alternative_japanische_lesungen, :chinesisch, :deutsche_uebersetzung, :eigene_ergaenzungen, :jahreszahlen, :japanische_umschrift, :kanji, :kennzahl, :koreanisch, :lemma_art, :literatur, :literatur_ergaenzungen, :pali, :quellen, :quellen_ergaenzungen, :sanskrit, :schreibvarianten, :spaltenzahl, :tibetisch, :uebersetzung, :namenskuerzel, :weitere_sprachen, :freigeschaltet, :order
+  attr_accessible :alternative_japanische_lesungen, :chinesisch, :deutsche_uebersetzung, :eigene_ergaenzungen, :jahreszahlen, :japanische_umschrift, :kanji, :kennzahl, :koreanisch, :lemma_art, :literatur, :literatur_ergaenzungen, :pali, :quellen, :quellen_ergaenzungen, :sanskrit, :schreibvarianten, :spaltenzahl, :tibetisch, :uebersetzung, :namenskuerzel, :weitere_sprachen, :freigeschaltet, :romaji_order
 
   before_save :cleanup
 
@@ -13,7 +13,7 @@ class Entry < ActiveRecord::Base
   def cleanup
     substituter = Substituter.new
     if self.japanische_umschrift
-      self.order = substituter.substitute(self.japanische_umschrift).downcase
+      self.romaji_order = substituter.substitute(self.japanische_umschrift).downcase
     end
   end
 
