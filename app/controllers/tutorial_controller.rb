981ae26573f900ef2822d364c2cf9eb3a5622cd6
thebluber <thebluber@gmail.com>
2013-07-07 03:50:51 +0200

    add tutorial

---
+++ app/controllers/tutorial_controller.rb
@@ -0,0 +1,8 @@
+class TutorialController < ApplicationController
+  before_filter :authenticate_user!
+  def index
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+end
