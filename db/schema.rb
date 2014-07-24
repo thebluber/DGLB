eee2504ac4e5b0d69a5ff1483cbb2fb5ade00640
jiayi.zheng@student.uni-tuebingen.de <thebluber@gmail.com>
2014-07-24 20:40:28 +0200

    Update import_html task: save lemmata as EntryHtml

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130531151801) do
+ActiveRecord::Schema.define(:version => 20140723133917) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -66,6 +66,61 @@ ActiveRecord::Schema.define(:version => 20130531151801) do
     t.string   "page_reference"
     t.boolean  "freigeschaltet",                  :default => false
     t.string   "romaji_order"
+  end
+
+  create_table "entry_docs", :force => true do |t|
+    t.string   "namenskuerzel"
+    t.string   "kennzahl"
+    t.string   "spaltenzahl"
+    t.string   "japanische_umschrift"
+    t.string   "kanji"
+    t.string   "pali"
+    t.string   "sanskrit"
+    t.string   "chinesisch"
+    t.string   "tibetisch"
+    t.string   "koreanisch"
+    t.string   "weitere_sprachen"
+    t.string   "alternative_japanische_lesungen"
+    t.string   "schreibvarianten"
+    t.string   "deutsche_uebersetzung"
+    t.string   "lemma_art"
+    t.string   "jahreszahlen"
+    t.text     "uebersetzung"
+    t.text     "quellen"
+    t.text     "literatur"
+    t.text     "eigene_ergaenzungen"
+    t.text     "quellen_ergaenzungen"
+    t.text     "literatur_ergaenzungen"
+    t.integer  "entry_id"
+    t.string   "romaji_order"
+    t.string   "page_reference"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+  end
+
+  create_table "entry_htmls", :force => true do |t|
+    t.string   "namenskuerzel"
+    t.string   "kennzahl"
+    t.string   "spaltenzahl"
+    t.string   "japanische_umschrift"
+    t.string   "kanji"
+    t.string   "pali"
+    t.string   "sanskrit"
+    t.string   "chinesisch"
+    t.string   "tibetisch"
+    t.string   "koreanisch"
+    t.string   "weitere_sprachen"
+    t.string   "alternative_japanische_lesungen"
+    t.string   "schreibvarianten"
+    t.string   "deutsche_uebersetzung"
+    t.string   "lemma_art"
+    t.string   "jahreszahlen"
+    t.text     "uebersetzung"
+    t.integer  "entry_id"
+    t.string   "romaji_order"
+    t.string   "page_reference"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
   end
 
   create_table "users", :force => true do |t|
